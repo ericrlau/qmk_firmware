54578e476621bc97d959ff1504b5b3498c0ee9c9
Eric Lau <eric.lau@specialized.com>
2020-05-18 09:09:35 -0600

    personal keymap

--- keyboards/ericrlau/numdiscipline/keymaps/ERL_Mac/keymap.c
+++ keyboards/ericrlau/numdiscipline/keymaps/ERL_Mac/keymap.c
@@ -19,18 +19,17 @@
 enum layer_names {
     _BASE,
     _FN,
-    _FN2
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BASE] = LAYOUT_65_2_right_mods(
+    [_BASE] = LAYOUT_std_2_right_mods(
         KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,            KC_DEL,   KC_PSLS, KC_PAST, KC_PMNS,
         MT(MOD_LALT | MOD_LSFT, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,            KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
         KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,                      KC_P4,    KC_P5,   KC_P6,   
         KC_LSPO,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC,           KC_UP,    KC_P1,    KC_P2,   KC_P3,   KC_PENT,
-        KC_LCTL, KC_LALT, KC_LGUI,                          LT(_FN2,KC_SPC),                 KC_RGUI, MO(1),              KC_LEFT, KC_DOWN,  KC_RIGHT, KC_P0,   KC_PDOT),
+        KC_LCTL, KC_LALT, KC_LGUI,                          MT(MOD_LGUI,KC_SPC),                 G(KC_SPC), MO(1),              KC_LEFT, KC_DOWN,  KC_RIGHT, KC_P0,   KC_PDOT),
 
-    [_FN] = LAYOUT_65_2_right_mods(
+    [_FN] = LAYOUT_std_2_right_mods(
      /*  esc      1        2        3        4       5       6       7       8       9         0        -         =     bkspc                 del           /         *       -   */
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,              KC_TRNS,   KC_PSLS, KC_PAST, KC_PMNS,
      /*  tab      Q        W        E        R        T      Y        U      I        O        P        [         ]        \                  7            8         9       +   */ 
@@ -40,18 +39,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /* shift             Z         X        C       V       B       N      M        ,        .        /        shift                 up       1            2         3        enter*/
         KC_TRNS,          KC_TRNS,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,           KC_VOLU,  KC_PGDN,    KC_P2,   KC_P3,   KC_PENT,
      /* ctrl     win      alt                              space                    command      fn                        left     down     right        0         .           */
-        KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS,                KC_TRNS, KC_TRNS,              KC_TRNS,KC_VOLD,  KC_TRNS,   KC_TRNS,   KC_TRNS     ),
-
-
-        [_FN2] = LAYOUT_65_2_right_mods(
-     /*  esc      1        2        3        4       5       6       7       8       9         0        -         =     bkspc                       del           /         *       -   */
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,                    KC_TRNS,   KC_PSLS, KC_PAST, KC_PMNS,
-     /*  tab      Q        W        E        R        T      Y        U      I        O        P        [         ]        \                        7            8         9       +   */ 
-        KC_TAB, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,  KC_TRNS, KC_BSLS,                    KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_PPLS,
-     /*  caps     A       S        D        F        G      H        J      K        L        ;        '       enter                                4            5         6           */
-        KC_CAPS, G(KC_A), G(KC_S), KC_TRNS, KC_UP,  KC_TRNS,KC_TRNS,KC_DOWN,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,  KC_TRNS,                              KC_PGUP,    KC_TRNS,   KC_TRNS,
-     /* shift             Z         X        C         V       B        N       M        ,        .         /        shift                  up       1            2         3       enter*/
-        KC_TRNS,          KC_TRNS,  G(KC_X), G(KC_C), G(KC_V), G(KC_B), KC_TRNS,KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,             KC_VOLU,  KC_PGDN,    KC_P2,   KC_P3,   KC_PENT,
-     /* ctrl     win      alt                              space                    command      fn                        left            down      right                         0         .           */
-        KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS,                KC_TRNS, KC_TRNS,                      (KC_LALT | KC_LSFT | KC_LEFT),KC_VOLD,  ((KC_LALT | KC_LSFT | KC_RIGHT))   , KC_TRNS, KC_TRNS)
+        KC_TRNS, KC_TRNS, KC_TRNS,                          KC_TRNS,                KC_TRNS, KC_TRNS,              RALT(RSFT(KC_LEFT)),KC_VOLD,  RALT(RSFT(KC_RIGHT)),   KC_TRNS,   KC_TRNS     )
 };
