96715da579eaa470dd033176ddb0bd795d1b646b
Eric Lau <eric.lau@specialized.com>
2020-11-07 15:04:38 -0700

    sync

--- keyboards/ericrlau/numdiscipline/keymaps/ERL_Mac_2RightMods/keymap.c
+++ keyboards/ericrlau/numdiscipline/keymaps/ERL_Mac_2RightMods/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MT(MOD_LALT | MOD_LSFT, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSPC,            KC_P7,    KC_P8,   KC_P9,   KC_PPLS,
         KC_LGUI, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,                      KC_P4,    KC_P5,   KC_P6,   
         KC_LSPO,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC,           KC_UP,    KC_P1,    KC_P2,   KC_P3,   KC_PENT,
-        KC_LCTL, KC_LALT, KC_LGUI,                          MT(MOD_LGUI,KC_SPC),                 G(KC_SPC), MO(1),              KC_LEFT, KC_DOWN,  KC_RIGHT, KC_P0,   KC_PDOT),
+        KC_LGUI, KC_LALT, KC_LGUI,                          KC_SPC,                 G(KC_SPC), MO(1),              KC_LEFT, KC_DOWN,  KC_RIGHT, KC_P0,   KC_PDOT),
 
     [_FN] = LAYOUT_std_2_right_mods(
      /*  esc      1        2        3        4       5       6       7       8       9         0        -         =     bkspc                 del           /         *       -   */
